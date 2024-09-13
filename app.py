9df8d0e1958efc671a142818957531f80f89bb0a
Kada SudhaGayathri <sudhagayathrikada@gmail.com>
2024-09-13 09:11:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+st.header("Book Recommender System")
+st.markdown('''
+#### The site using collaborative filtering suggests books from our catalog.
+#### We recommend the top 50 books for everyone as well.
+''')
